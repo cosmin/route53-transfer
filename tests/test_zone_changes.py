fe7fcd7d89f8bd7e6fc8c30ca1d9bbf9481db578
Cosimo Streppone <cosimo@kahoot.com>
2021-11-26 18:23:18 +0100

    Added basic unit tests for zone changes logic

---
+++ tests/test_zone_changes.py
@@ -0,0 +1,163 @@
+from boto.route53.record import Record
+
+from route53_transfer import app
+
+TEST_ZONE_ID = 1
+TEST_ZONE_NAME = "test.dev"
+TEST_ZONE = {"id": TEST_ZONE_ID, "name": TEST_ZONE_NAME}
+
+
+def diff_zone(rrset_before, rrset_after):
+    return app.compute_changes(TEST_ZONE, rrset_before, rrset_after)
+
+
+def test_empty_source_and_destination_zones():
+    rrset_before = []
+    rrset_after = []
+    changes = diff_zone(rrset_before, rrset_after)
+    assert len(changes) == 0
+
+
+def test_soa_and_ns_records_are_ignored():
+    """
+    SOA and NS records are ignored when computing zone changes
+    """
+
+    soa_rr = Record()
+    soa_rr.type = "SOA"
+    soa_rr.name = TEST_ZONE_NAME
+
+    rrset_before = []
+    rrset_after = [soa_rr]
+
+    changes = diff_zone(rrset_before, rrset_after)
+    assert len(changes) == 0
+
+    ns_rr = Record()
+    ns_rr.type = "NS"
+    ns_rr.name = TEST_ZONE_NAME
+
+    rrset_before = []
+    rrset_after = [ns_rr]
+
+    changes = diff_zone(rrset_before, rrset_after)
+    assert len(changes) == 0
+
+    rrset_after = [soa_rr, ns_rr]
+
+    changes = diff_zone(rrset_before, rrset_after)
+    assert len(changes) == 0
+
+
+def test_soa_and_ns_records_are_ignored():
+    """
+    SOA and NS records are ignored when computing zone changes
+    """
+
+    soa_rr = Record()
+    soa_rr.type = "SOA"
+    soa_rr.name = TEST_ZONE_NAME
+
+    rrset_before = []
+    rrset_after = [soa_rr]
+
+    changes = diff_zone(rrset_before, rrset_after)
+    assert len(changes) == 0
+
+    ns_rr = Record()
+    ns_rr.type = "NS"
+    ns_rr.name = TEST_ZONE_NAME
+
+    rrset_before = []
+    rrset_after = [ns_rr]
+
+    changes = diff_zone(rrset_before, rrset_after)
+    assert len(changes) == 0
+
+    rrset_after = [soa_rr, ns_rr]
+
+    changes = diff_zone(rrset_before, rrset_after)
+    assert len(changes) == 0
+
+    rrset_before = rrset_after = [soa_rr, ns_rr]
+
+    changes = diff_zone(rrset_before, rrset_after)
+    assert len(changes) == 0
+
+
+def test_add_one_simple_a_record():
+    a_ptr = Record()
+    a_ptr.type = "A"
+    a_ptr.name = "server1"
+    a_ptr.resource_records = ["1.2.3.4"]
+
+    rrset_before = []
+    rrset_after = [a_ptr]
+
+    changes = diff_zone(rrset_before, rrset_after)
+
+    assert changes == [
+        {
+            "operation": "CREATE",
+            "zone": TEST_ZONE,
+            "record": a_ptr,
+        }
+    ]
+
+
+def test_replace_simple_a_record():
+    server1_ptr = Record()
+    server1_ptr.type = "A"
+    server1_ptr.name = "server1"
+    server1_ptr.resource_records = ["1.2.3.4"]
+
+    modified_server1_ptr = Record()
+    modified_server1_ptr.type = "A"
+    modified_server1_ptr.name = "server1"
+    modified_server1_ptr.resource_records = ["1.2.3.5"]
+
+    rrset_before = [server1_ptr]
+    rrset_after = [modified_server1_ptr]
+
+    changes = diff_zone(rrset_before, rrset_after)
+
+    assert changes == [
+        {
+            "operation": "DELETE",
+            "zone": TEST_ZONE,
+            "record": server1_ptr,
+        },
+
+        {
+            "operation": "CREATE",
+            "zone": TEST_ZONE,
+            "record": modified_server1_ptr,
+        }
+    ]
+
+
+def test_add_a_record_to_existing_zone():
+    server1_ptr = Record()
+    server1_ptr.type = "A"
+    server1_ptr.name = "server1"
+    server1_ptr.resource_records = ["1.2.3.4"]
+
+    rrset_before = [server1_ptr]
+
+    server2_ptr = Record()
+    server2_ptr.type = "CNAME"
+    server2_ptr.name = "server2"
+    server2_ptr.resource_records = ["server2.another.zone.dev"]
+
+    rrset_after = [server1_ptr, server2_ptr]
+
+    changes = diff_zone(rrset_before, rrset_after)
+
+    assert changes == [
+        {
+            "operation": "CREATE",
+            "zone": TEST_ZONE,
+            "record": server2_ptr,
+        }
+    ]
+
